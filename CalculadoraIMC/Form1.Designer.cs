d5e09578145ee5f736478128994db8c438dd2915
LuizAlisson <alysson.luiz.0708@gmail.com>
2025-08-18 22:02:30 -0300

    Implementação do código
    Foi implementado o cálculo do IMC e as váriaveis, feita interface com uso de imagens e tratamento de erros.

--- CalculadoraIMC/Form1.Designer.cs
+++ CalculadoraIMC/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalculadoraIMC
 {
-    partial class Form1
+    partial class AchouQÑianomearessePNG
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.pctTabela = new System.Windows.Forms.PictureBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblCondição = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(112, 41);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(52, 13);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso(KG)";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(115, 87);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(49, 13);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura(M)";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(219, 38);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(219, 84);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(219, 124);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 29);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // pctTabela
+            // 
+            this.pctTabela.Image = global::CalculadoraIMC.Properties.Resources.IMC_tabela;
+            this.pctTabela.Location = new System.Drawing.Point(372, 28);
+            this.pctTabela.Name = "pctTabela";
+            this.pctTabela.Size = new System.Drawing.Size(375, 162);
+            this.pctTabela.TabIndex = 5;
+            this.pctTabela.TabStop = false;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(151, 231);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(26, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "IMC";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(219, 228);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // lblCondição
+            // 
+            this.lblCondição.AutoSize = true;
+            this.lblCondição.Location = new System.Drawing.Point(151, 297);
+            this.lblCondição.Name = "lblCondição";
+            this.lblCondição.Size = new System.Drawing.Size(52, 13);
+            this.lblCondição.TabIndex = 8;
+            this.lblCondição.Text = "Condição";
+            // 
+            // AchouQÑianomearessePNG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCondição);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.pctTabela);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Name = "AchouQÑianomearessePNG";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctTabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.PictureBox pctTabela;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblCondição;
     }
 }
 
--- CalculadoraIMC/Form1.cs
+++ CalculadoraIMC/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,100 @@ using System.Windows.Forms;
 
 namespace CalculadoraIMC
 {
-    public partial class Form1 : Form
+    public partial class AchouQÑianomearessePNG : Form
     {
-        public Form1()
+        public AchouQÑianomearessePNG()
         {
             InitializeComponent();
+        }
+
+        private void txtPeso_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void txtAltura_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double resultado = 0;
+            double peso;
+            double altura;
+
+            if (!double.TryParse(txtPeso.Text, out peso))
+            //Double tryparse transforma a string em Double e se der certo, roda o if
+            //com o ! na frente para verificar se é diferente
+            {
+                MessageBox.Show("Digite um valor válido para o peso!", "Inválido",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+                //Retorna sem executar o resto do programa
+
+            }
+
+            else if (!double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("Digite um valor válido para o altura!", "Inválido",
+                       MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+                //Retorna sem executar o resto do programa
+
+            }
+            else if (altura <= 0)
+            //Verifica se o valor da altura é maior que zero
+            {
+                MessageBox.Show("A altura deve ser maior que zero!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                resultado = peso / (altura * altura);
+                txtResultado.Text = resultado.ToString("F2");
+                //F2 formata para duas casas decimais
+            }
+
+            if (resultado < 18.5)
+            {
+                lblCondição.Text = ("Abaixo do peso");
+            }
+
+            else if (resultado <= 24.9)
+            {
+                lblCondição.Text = ("Peso ideal");
+                MessageBox.Show("VOCÊ ESTÁ NO PESO IDEAL", "PARABÉNS",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            else if (resultado <= 29.9)
+            {
+                lblCondição.Text = ("Levemente acima do peso");
+            }
+
+            else if (resultado <= 34.9)
+            {
+                lblCondição.Text = ("Obesidade grau I");
+            }
+
+            else if (resultado <= 39.9)
+            {
+                lblCondição.Text = ("Obesidade grau II (severa)");
+            }
+
+            else if (resultado < 40)
+            {
+                lblCondição.Text = ("Obesidade III (Mórbida)");
+            }
+
+
+
         }
     }
 }
